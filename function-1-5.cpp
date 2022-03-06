1724605ba2045e7166436316597edcdd2a29ac0b
Lee <100659748+ZiyuanLi726@users.noreply.github.com>
2022-03-06 15:17:47 +0800

    Update function-1-5.cpp

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int count_even(int number)
 {
     int total = 0;
-    for(int i = 1; i < number; i++)
+    for(int i = 1; i <= number; i++)
     {
         if (i % 2 == 0)
         {
-            total+=i;
+            total++;
         }
     }
     return total;
