7cbddb4f9f09b6bfd1e783ab96c08f89e1ef35a5
Lee <100659748+ZiyuanLi726@users.noreply.github.com>
2022-03-06 17:03:51 +0800

    Update function-2-3.cpp

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -6,21 +6,25 @@ void twofivenine(int array[], int n)
     int two = 0;
     int five = 0;
     int nine = 0;
+    
     for (int i = 0; i < n; i++)
     {
-        if (array[i] == 2)
+        switch (array[i])
         {
-            two+=1;
-        }
-        else if (array[i] == 5)
-        {
-            five+=1;
-        }
-        else if (array[i] == 9)
-        {
-            nine+=1;
+            case 2:
+            two++;
+            break;
+            
+            case 5:
+            five++;
+            break;
+            
+            case 9:
+            nine++;
+            break;
         }
     }
+   
     cout << "2:" << two << ";" << "5:" << five << ";" << "9:" << nine << ";" << endl;
     cout<<endl;
 }
