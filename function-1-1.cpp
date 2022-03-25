c5a8aeabb88fc10cacb3d1b862dbff7ea6834f1d
Lee <100659748+ZiyuanLi726@users.noreply.github.com>
2022-03-25 17:24:30 +0800

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -10,12 +10,4 @@ void copy_2d_strings(std::string first[][2], std::string second[][2], int n)
 			second[i][j] = first[i][j];
 		}
 	}
-
-	for (int i = 0; i < n; i++) 
-	{
-		for (int j = 0; j < 2; j++) 
-		{
-			cout << second[i][j] << endl;
-		}
-	}
 }
