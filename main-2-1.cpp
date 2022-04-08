f10291f928155088cc740af3aeb80bd3f7a3c511
Lee <100659748+ZiyuanLi726@users.noreply.github.com>
2022-04-08 18:51:39 +0800

    Update main-2-1.cpp

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <stdlib.h>
+#include <string>
+#include "meerkat.h"
 #include "cart.h"
 using namespace std;
-
-int main()
+int main(int argc, char const *argv[])
 {
-    
+meerkat Alice;
+Alice.setName("Alice");
+Alice.setAge(19);
+meerkat Sam;
+Sam.setName("Sam");
+Sam.setAge(19);
+
+
+cart cart;
+meerkat M[2]={Alice,Sam};
+
+for (size_t i = 0; i < 2; i++)
+{
+
+if (cart.addMeerkat(M[i])==false)
+{
+
+std::cout << "the cart is full" <<std::endl;
+}
+}
+
+cart.printMeerkats();
+return 0;
 }
