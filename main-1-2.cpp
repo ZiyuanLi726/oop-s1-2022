c934a8ddb6c5f1392030cf96cd506bb40e179fd2
Lee <100659748+ZiyuanLi726@users.noreply.github.com>
2022-04-29 13:10:22 +0800

    Update main-1-2.cpp

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    Apartment r1(10);
+    Aparment r1(10);
     
     Unit a(4,39,100);
     Unit b(5,30,200);
@@ -11,5 +11,7 @@ int main()
     Unit d(7,36,400);
     Unit e(8,31,500);
     
-    
+    r1.get_capacity();
+    r1.get_current_number_of_units();
+    r1.add_unit(a);
 }
