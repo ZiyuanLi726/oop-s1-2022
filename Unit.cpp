e7aad18cfde399d6c3c215c19d50db282c27ff0b
Lee <100659748+ZiyuanLi726@users.noreply.github.com>
2022-04-29 12:55:12 +0800

    Update Unit.cpp

--- Unit.cpp
+++ Unit.cpp
@@ -18,18 +18,27 @@ Unit::Unit(int, double, int)
 
 int Unit::get_bedrooms()
 {
-    
+    if(bednum<0)
+    {
+        bednum = 0;
+    }
     return bednum;
 }
 
 int Unit::get_value()
 {
-   
+    if(price<0)
+    {
+        price = 0;
+    }
     return price;
 }
 
 double Unit::get_area()
 {
-   
+    if(square<0)
+    {
+        square = 0;
+    }
     return square;
 }
