6c01e6aa1846477e7f7ef7267b96131c51d358bb
Lee <100659748+ZiyuanLi726@users.noreply.github.com>
2022-06-03 13:18:48 +0800

    Update main-2-1.cpp

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,17 +1,1 @@
 
-#include "Dymocks.h"
-#include <iostream>
-#include <string>
-using namespace std;
-
-
-
-int main()
-{
-    Dymocks online_shop(1);
-    online_shop.set_name("OL_shop");
-    cout << online_shop.get_name() << endl;
-    cout << online_shop.get_onlineTax() << endl;
-    cout << online_shop.get_numBook() << endl;
-    cout << online_shop.get_isOnline() << endl;
-}
