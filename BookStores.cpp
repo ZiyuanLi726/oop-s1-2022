91a347169bd16c5e57c55286e6931758c2b3eb33
Lee <100659748+ZiyuanLi726@users.noreply.github.com>
2022-06-03 13:16:04 +0800

    Update BookStores.cpp

---
+++ BookStores.cpp
@@ -0,0 +1,18 @@
+#include "BookStores.h"
+
+void BookStores::sort(Dymocks** store, int n)
+{
+    Dymocks* temp = nullptr;
+    for(int i=0;i<n;i++)
+	{		
+		for(int j=i+1;j<n;j++)
+		{
+			if (store[i]->get_storeID() > store[j]->get_storeID())
+			{
+				temp = store[i];
+				store[i] = store[j];
+				store[j] = temp;
+			}
+		}
+	}
+}
