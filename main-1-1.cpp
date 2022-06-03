1434aa047b78deba072e820aaddddbc10e2f3b87
Lee <100659748+ZiyuanLi726@users.noreply.github.com>
2022-06-03 13:18:36 +0800

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
-#include <string>
-using namespace std;
-#include "BookStore.h"
 
-int main()
-{
-    BookStore shop("shop", "111 baker street", 100);
-    cout << shop.get_address() << endl;
-    shop.set_address("22 taminga way");
-    cout << shop.get_address() << endl;
-    return 0;
-}
