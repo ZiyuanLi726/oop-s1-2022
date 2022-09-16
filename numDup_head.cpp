2805f7345c370f83b92d517e02e3ac89f21693d5
Lee <100659748+ZiyuanLi726@users.noreply.github.com>
2022-09-16 14:42:46 +0800

    Add files via upload

---
+++ numDup_head.cpp
@@ -0,0 +1,15 @@
+int numDup(int arr[], int start, int size, int element) {
+    int dup = 0;
+    if(start < 0)
+    {
+        return 0;
+    }
+    
+    if(start==element)
+    {
+        ++dup;
+    }
+    
+
+    return (dup+numDup(arr[], start-1, size, element));
+}
